512e0a09bf6a0852f7d76118ca27d63af102ad9e
retraigo <nett@nett.moe>
2023-03-24 16:38:52 +0530

    council

--- pages/success.vue
+++ pages/success.vue
@@ -1,5 +1,5 @@
 <template>
     <NuxtLayout>
-        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">You are <span class = "line-through text-zinc-500">bot</span> bread 👍</div>
+        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">The council has decided. You are <span class = "line-through text-zinc-500">bot</span> bread 👍</div>
     </NuxtLayout>
 </template>
