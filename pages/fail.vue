a6e25ff814ed8424da74e779ebccb3e3eec46537
retraigo <nett@nett.moe>
2023-03-24 16:32:35 +0530

    you are bread

--- pages/fail.vue
+++ pages/fail.vue
@@ -1,5 +1,5 @@
 <template>
     <NuxtLayout>
-        <div class="mt-8 mx-auto max-w-6xl text-4xl font-semibold uppercase">Incorrect Captcha</div>
+        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">You are <span class = "line-through text-zinc-500">bot</span> bread 👍</div>
     </NuxtLayout>
 </template>
