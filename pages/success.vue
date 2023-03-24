563bc56ed86bd60a0a56ff5ce1984b5d56fd612c
retraigo <nett@nett.moe>
2023-03-24 16:27:42 +0530

    welcome to kcg

--- pages/success.vue
+++ pages/success.vue
@@ -1,5 +1,9 @@
 <template>
     <NuxtLayout>
-        <div class="mt-8 mx-auto max-w-6xl text-4xl font-semibold uppercase">Success</div>
-    </NuxtLayout>
-</template>
+        <div class="mt-8 mx-auto max-w-6xl text-xl font-semibold uppercase text-justify">CYBERCRATZ 1. O is a National Level Cyber
+            Hackathon organized by Department of Information Technology, KCG College of Technology in collaboration with
+            Computer Society of India (CSI)& The Institution of Engineering & Technology (IET), to provide students with a
+            platform to solve problems we face in our daily lives, a culture of product innovation and out-of-the-box
+        thinking in young minds, exclusively for engineering students from across India. Build innovative accelerated
+        applications, grow your network, meet Cybersecurity experts, and grab exciting prizes.</div>
+</NuxtLayout></template>
