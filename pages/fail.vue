1aeffe5bc507fc21c1c00e9dc7144c559529afff
retraigo <nett@nett.moe>
2023-03-24 18:13:45 +0530

    emphasize

--- pages/fail.vue
+++ pages/fail.vue
@@ -1,5 +1,5 @@
 <template>
     <NuxtLayout>
-        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">You are <span class = "font-black">bot</span></div>
+        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">You. are. <span class = "font-black">bot</span>.</div>
         <img src="/RAGE.gif" class="mx-auto w-full lg:w-72 p-4" />
 </NuxtLayout></template>
