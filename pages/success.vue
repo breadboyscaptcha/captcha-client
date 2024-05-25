8c09a1ec23a328e44423d34a4a3bb059b35dec51
Pranev (NeTT) <nett@nett.moe>
2024-05-25 11:19:37 +0530

    Update success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -1,5 +1,6 @@
 <template>
     <NuxtLayout>
-        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">The council has decided. You are <span class = "line-through text-zinc-500">bot</span> bread 👍</div>
-    </NuxtLayout>
+        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">Authenticated</div>
+<!--        <div class="mt-8 mx-auto max-w-6xl text-4xl text-center font-semibold uppercase">The council has decided. You are <span class = "line-through text-zinc-500">bot</span> bread 👍</div>
+-->    </NuxtLayout>
 </template>
